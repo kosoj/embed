58a32db249282feaa09d4f52e98014e60c291d09
Daniel Steigerwald <daniel@steigerwald.cz>
2014-08-14 13:59:19 +0200

    Add links in header.

--- app/client/js/react/header.jsx
+++ app/client/js/react/header.jsx
@@ -3,8 +3,15 @@ goog.provide('app.react.Header');
 app.react.Header = React.createClass({
 
   render: function() {
+    var routes = this.props.routes;
+
     return (
-      <header>Header</header>
+      <header>Header
+        <ul>
+          <li><a href={routes.home.createUrl()}>Home</a></li>
+          <li><a href={routes.contracts.createUrl()}>Contracts</a></li>
+        </ul>
+      </header>
     );
   }
 
