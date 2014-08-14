68ce73e4665b020589c7ee502c202d55146b552b
Daniel Steigerwald <daniel@steigerwald.cz>
2014-08-14 13:58:51 +0200

    Add home and contracts pages.

---
+++ app/client/js/react/pages/contracts.jsx
@@ -0,0 +1,13 @@
+goog.provide('app.react.pages.Contracts');
+
+app.react.pages.Contracts = React.createClass({
+
+  render: function() {
+    return (
+      <div className="contracts">
+        <p>TODO: Search form. And listing.</p>
+      </div>
+    );
+  }
+
+});
---
+++ app/client/js/react/pages/home.jsx
@@ -0,0 +1,13 @@
+goog.provide('app.react.pages.Home');
+
+app.react.pages.Home = React.createClass({
+
+  render: function() {
+    return (
+      <div className="home">
+        <p>Home page.</p>
+      </div>
+    );
+  }
+
+});
