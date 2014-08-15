854d65396c55a9a353ca032de8e6400fd7d74edf
Daniel Steigerwald <daniel@steigerwald.cz>
2014-08-15 12:14:49 +0200

    Make contractsStore preloaded.

--- app/client/js/contracts/store.jsx
+++ app/client/js/contracts/store.jsx
@@ -1,29 +1,40 @@
 goog.provide('app.contracts.Store');
 
 goog.require('goog.Promise');
+goog.require('goog.array');
 
 /**
   @constructor
 */
 app.contracts.Store = function() {
-
-}
-
-/**
-  @param {app.contracts.Contract} contract
-  @return {!goog.Promise}
-*/
-app.contracts.Store.prototype.search = function(contract) {
-  var contracts = [
+  this.contracts = [
     {number: 1, ident: 11, text: 'Hoří má panenko.'},
     {number: 2, ident: 22, text: 'Smlouva s ďáblem.'},
     {number: 3, ident: 33, text: 'Společenská smlouva.'},
     {number: 4, ident: 44, text: 'To by mělo stačit.'}
-  ]
+  ];
+}
 
+/**
+  TODO: ulozi contrct na sobě, takže si to komponenta, sosne ze store
+  @param {app.contracts.Contract} searchFilter
+  @return {!goog.Promise}
+*/
+app.contracts.Store.prototype.search = function(searchFilter) {
+  // TODO: Search filter.
   return new goog.Promise(function(resolve, reject) {
     setTimeout(function() {
-      resolve(contracts);
-    }, 3000);
-  })
+      resolve(this.contracts);
+    }.bind(this), 1000);
+  }, this);
+}
+
+/**
+  @param {string} number Yeah, that's what I call great props namming ;)
+  @return {app.contracts.Contract}
+*/
+app.contracts.Store.prototype.getContractByNumber = function(number) {
+  return goog.array.find(this.contracts, function(contract) {
+    return contract.number == number;
+  });
 }
